a509e0cfea9a76a5371e0043c31baf8cfd44a951
Andrew Zeiss <azeiss9@gmail.com>
2024-06-28 12:00:41 -0400

    Initial Commit

---
+++ whimsApp/whimsApp/whimsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  whimsAppApp.swift
+//  whimsApp
+//
+//  Created by Andrew Zeiss on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct whimsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
