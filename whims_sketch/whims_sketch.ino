40df35ed6d409eae426ff673d58a95fc8c3f5672
Andrew Zeiss <azeiss9@gmail.com>
2024-07-09 15:56:32 -0400

    Succesfully awaits command from server and conducts a single cycle

--- whims_sketch/whims_sketch.ino
+++ whims_sketch/whims_sketch.ino
@@ -1,123 +1,116 @@
 #include <ArduinoBLE.h>
 
-byte oldHR = 0;
-BLECharacteristic heartRateMeasurement;
+#define vibePin1 2
+#define vibePin1 3
+#define vibePin1 4
+#define vibePin1 5
+#define vibePin1 6
 
+#define DEBUG true
+
+int vibePins[5] = { 2, 3, 4, 5, 6 };
+int i, j, p, pos;
+bool approved, questionAnswered = false, readyToContinueToNext = false;
+
+int num_of_vibes = sizeof(vibePins) / sizeof(vibePins[0]);  //stores the # of items in servoArray
+unsigned long runTime = 15000; //5000
+unsigned long vibeRunTime = runTime / sizeof(vibePins);
+unsigned long delayTime = runTime / 10, halfDelay = delayTime / 2;
+
+// These UUIDs have been randomly generated. - they must match between the Central and Peripheral devices
+// Any changes you make here must be suitably made in the Python program as well
+
+BLEService nanoService("13012F00-F8C3-4F4A-A8F4-15CD926DA146"); // BLE Service
+
+// BLE Characteristics - custom 128-bit UUID, read and writable by central device
+BLEByteCharacteristic hapticCharacteristic("0000ffe1-0000-1000-8000-00805f9b34fb", BLERead | BLEWrite);
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+    Serial.begin(9600);
 
-  if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
-    while (1);
-  }
+        if (DEBUG) {
+      while(!Serial);
+    }
+    
+   // begin initialization
+    if (!BLE.begin()) {
+        Serial.println("Starting BLE failed!");
+        while (1);
+    }
 
-  Serial.println("BLE Central - Scanning for peripherals...");
-  BLE.scan();
+    // set advertised local name and service UUID:
+    BLE.setLocalName("Arduino Nano 33 IoT");
+    BLE.setAdvertisedService(nanoService);
 
+    // add the characteristic to the service
+    nanoService.addCharacteristic(hapticCharacteristic);
+
+    // add service
+    BLE.addService(nanoService);
+
+    // set the initial value for the characeristic:
+    hapticCharacteristic.writeValue(0);
+
+    // start advertising
+    BLE.advertise();
+    delay(100);
+    Serial.println("WHIMS Controller is online!");
 }
 
 void loop() {
-  // Check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
+    // listen for BLE centrals to connect:
+    BLEDevice central = BLE.central();
 
+    // if a central is connected to peripheral:
+    if (central) {
+        Serial.print("Connected to central: ");
+        // print the central's MAC address:
+        Serial.println(central.address());
 
-  BLECharacteristic heartRate = peripheral.characteristic("00002A37-0000-1000-8000-00805f9b34fb");
-
-
-  //"6a4e310-667B-11e3-949a-0800200c9a66"
-
-  if (peripheral) {
-    // Print the discovered peripheral address
-    Serial.print("Discovered peripheral: ");
-    Serial.println(peripheral.address());
-
-    // Attempt to connect to the peripheral
-    //Specific to my heart rate monitor
-    if (peripheral.localName() == "HRMPro+:192052" || peripheral.address() == "e8:5c:11:c3:66:0b") {
-      Serial.print("Connecting to peripheral: ");
-      Serial.println(peripheral.address());
-
-      // Stop scanning while connecting
-      BLE.stopScan();
-
-      if (peripheral.connect()) {
-        Serial.println("Connected to peripheral");
-      } else {
-        Serial.println("Failed to connect");
-        BLE.scan(); // Resume scanning if connection fails
-      }
-      if (peripheral.discoverAttributes()) {
-        Serial.println("Attributes Discovered");
-/*
-        int serviceCount = peripheral.serviceCount();
-        for (int i = 0; i < serviceCount; i++) {
-          BLEService service = peripheral.service(i);
-          Serial.print("Service UUID: ");
-          Serial.println(service.uuid());
-
-          int characteristicCount = service.characteristicCount();
-          for (int j = 0; j < characteristicCount; j++) {
-            BLECharacteristic characteristic = service.characteristic(j);
-            Serial.print("  Characteristic UUID: ");
-            Serial.println(characteristic);
-            Serial.print("  Properties: ");
-            Serial.println(characteristic.properties());
-          }
+        // while the central is still connected to peripheral:
+        while (central.connected()) {
+            if (hapticCharacteristic.written()) {
+                if (hapticCharacteristic.value()) {   
+                    Serial.println("Haptic signal recieved!");
+                    SingleCycle();
+                    hapticCharacteristic.writeValue(0);
+                }
+            }
         }
-      } else {
-        Serial.println("Attribute discovery failed");
-*/
+        // when the central disconnects, print it out:
+        Serial.print(F("Disconnected from central: "));
+        Serial.println(central.address());
+    }
+}
 
-        BLEService heartRateService = peripheral.service("180D");
-        if (heartRateService) {
-          heartRateMeasurement = heartRateService.characteristic("2A37");
-          if (heartRateMeasurement) {
-            heartRateMeasurement.subscribe();
-            Serial.println("Subscribed to heart Rate measurement!!!");
-          } else {
-            Serial.println("Heart Rate measurement subscription failed...");
-          }
-        }
-      }
 
-/*
-      Serial.println("Subscribing to heart rate monitor... ");
-      if (!heartRate) {
-        Serial.println("no heart rate characteristic found");
-        peripheral.disconnect();
-        return;
-      } else if (!heartRate.canSubscribe()) {
-        Serial.println("Heart rate is not subscribable");
-        peripheral.disconnect();
-        return;
-      } else if (!heartRate.subscribe()) {
-        Serial.println("Subscription failed");
-        peripheral.disconnect();
-        return;
-      }
-*/
-
-      while (peripheral.connected()) {
-
-        if (heartRateMeasurement.valueUpdated()) {
-
-          byte heartRateData[2];
-          heartRateMeasurement.readValue(heartRateData, 2);
-
-          byte currentHR = heartRateData[1];
-
-          if (oldHR != currentHR) {
-            oldHR = currentHR;
-            Serial.print("Heart Rate: ");
-            Serial.println(currentHR);
-          }
-        }
-      }
-      // Resume scanning
-      BLE.scan();
+void SingleCycle() {
+  Serial.println(num_of_vibes);
+  for (i = 0; i <= num_of_vibes - 1; i++) {
+    Serial.println(i + 1);
+    if (i == 0) {
+      digitalWrite(vibePins[i], HIGH);
+      delay(delayTime);
+    } else {
+      digitalWrite(vibePins[i - 1], LOW);
+      digitalWrite(vibePins[i], HIGH);
+      delay(delayTime);
     }
   }
+  for (p = 0; p <= num_of_vibes - 1; p++) {
+    digitalWrite(vibePins[p], LOW);
+  }
+  delay(delayTime);
+  for (i = num_of_vibes - 1; i >= 0; i--) {
+    Serial.println(i + 1);
+    digitalWrite(vibePins[i + 1], LOW);
+    digitalWrite(vibePins[i], HIGH);
+    delay(delayTime);
+  }
+  for (p = 0; p <= num_of_vibes - 1; p++) {
+    digitalWrite(vibePins[p], LOW);
+  }
 }
+
+
 
 
